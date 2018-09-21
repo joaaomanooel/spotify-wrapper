f6c5ebd4c8b636c38ba756d4afd45a4908579a10
João Manoel Neto <joaaomanooel@gmail.com>
2018-09-21 16:23:40 -0300

    Bump version 1.0.1

--- src/album.js
+++ src/album.js
@@ -1,14 +1,14 @@
-import API_URL from './config';
+import { API_URL, HEADERS } from './config';
 import toJSON from './utils';
 
 const getAlbum = id =>
-  fetch(`${API_URL}/albums/${id}`).then(toJSON);
+  fetch(`${API_URL}/albums/${id}`, HEADERS).then(toJSON);
 
 const getAlbums = id =>
-  fetch(`${API_URL}/albums/?ids=${id}`).then(toJSON);
+  fetch(`${API_URL}/albums/?ids=${id}`, HEADERS).then(toJSON);
 
 const getAlbumTraks = id =>
-  fetch(`${API_URL}/albums/${id}/tracks`).then(toJSON);
+  fetch(`${API_URL}/albums/${id}/tracks`, HEADERS).then(toJSON);
 
 export {
   getAlbum,
--- src/config.js
+++ src/config.js
@@ -1,3 +1,14 @@
 const API_URL = 'https://api.spotify.com/v1';
 
-export default API_URL;
+const TOKEN_API = 'BQCLPWg5gVOVkv5FwTKtMKDQ6ItIZ5R8G32sXWes6R4XJzk0n-NEf0LKO68XikFqCDirq2OlvHfODceLMzQTUuA7rcX5S7749UPzd9bH43ZtXbJQxfdEM_Zh4f9Lo2gK4qBnNKYEwV3g1DrbF_p0IAZOK-4XdjeEUIHM7B7_c_pBb5GT3rVwdcEmmLGDcRoVphrrIOql0t-66gxp5sKvN3o5fpW4T1dHA2esdK-zCPpax3Fr6dUHpYH_utsSb2TWQzCFfxmA_Dw2RBt9qbXiU6_p6z9O9rRjJkH18cQ';
+
+const HEADERS = {
+  headers: {
+    Authorization: `Bearer ${TOKEN_API}`,
+  },
+};
+
+export {
+  API_URL,
+  HEADERS,
+};
